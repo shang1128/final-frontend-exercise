e380ddf3fe4a39d944efee7c2de0c7961bab51ff
Victor Chiong <cjvicro@gmail.com>
2021-05-05 21:30:02 +0800

    JS changes || click Raku to open newsletter

--- js/index.js
+++ js/index.js
@@ -2,7 +2,7 @@
 
 let popupCloseBtn = document.querySelector('.close');
 
-let title = document.querySelector('.title');
+let title = document.querySelector('.title h1');
 
 title.addEventListener("click",()=>{
     document.querySelector('.newsletter').style.display = "flex";
