5ac6482283635e088a38ddae6bdfb47ac202d7d8
Victor Chiong <cjvicro@gmail.com>
2021-05-05 22:43:02 +0800

    added JS functionality to page 11

--- js/page11.js
+++ js/page11.js
@@ -3,10 +3,28 @@ let quantityInput = document.querySelectorAll('.quantity');
 let upQuantity = document.querySelectorAll('.add-quantity');
 let downQuantity = document.querySelectorAll('.minus-quantity');
 
+let price = document.querySelectorAll('.price');
+let unitPrice = document.querySelectorAll('.unit-price');
+let unitPriceHidden = document.querySelectorAll('.unit-price-hidden');
+
+function calculateTotal(){
+    let subtotal = document.querySelector('.sub-total');
+    let totalPrice = document.querySelector('.total-price');
+    let subtotalVal = 0;
+    unitPriceHidden.forEach((elem,index)=>{
+        subtotalVal += parseInt(elem.value);
+    });
+    subtotal.innerText = `$${subtotalVal}`;
+    totalPrice.innerText = `$${subtotalVal + 20}`;
+}
+
 upQuantity.forEach((elem, index) => {
     elem.addEventListener("click", (e)=>{
         e.preventDefault();
         quantityInput[index].value++;
+        unitPrice[index].innerText = `$${(quantityInput[index].value * price[index].value)}`; 
+        unitPriceHidden[index].value = (quantityInput[index].value * price[index].value);
+        calculateTotal();
     });
 });
 
@@ -15,6 +33,8 @@ downQuantity.forEach((elem, index) => {
         e.preventDefault();
         if(quantityInput[index].value > 0)
             quantityInput[index].value--;
+            unitPrice[index].innerText = `$${(quantityInput[index].value * price[index].value)}`; 
+            calculateTotal();
     });
 });
 
@@ -27,5 +47,8 @@ itemDelete.forEach((elem,index)=>{
     elem.addEventListener("click", ()=>{
         items[index].style.display = "none";
         hr[index].style.display = "none";
+        price[index].value = 0;
+        unitPriceHidden[index].value = 0;
+        calculateTotal();
     });
 });
