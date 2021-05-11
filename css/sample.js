7a8c3126dcbb256a404c44f146c3b2353243ec7d
shang1128 <centrii52@gmail.com>
2021-05-11 14:49:59 +0800

    add javascript

--- css/sample.js
+++ css/sample.js
@@ -23,3 +23,29 @@ function assignedGrade(score){
  
 }
 console.log(assignedGrade(63));
+
+document.getElementsByClassName('blue')[0].addEventListener('click', function(){
+    document.getElementsByClassName('items-left-details--productImages--largeView')[0].style="background-color:blue";
+})
+document.getElementsByClassName('red')[0].addEventListener('click', function(){
+    document.getElementsByClassName('items-left-details--productImages--largeView')[0].style="background-color:red";
+})
+document.getElementsByClassName('black')[0].addEventListener('click', function(){
+    document.getElementsByClassName('items-left-details--productImages--largeView')[0].style="background-color:black";
+})
+document.getElementsByClassName('yellow')[0].addEventListener('click', function(){
+    document.getElementsByClassName('items-left-details--productImages--largeView')[0].style="background-color:yellow";
+})
+
+let counter=1;
+
+document.getElementsByClassName('numOrder--add')[0].addEventListener('click', function(){
+    counter++
+    document.getElementsByClassName('numOrder--value')[0].innerHTML=counter;
+})
+
+document.getElementsByClassName('numOrder--sub')[0].addEventListener('click', function(){
+if (counter <= 0) return;
+counter--
+document.getElementsByClassName('numOrder--value')[0].innerHTML=counter;
+})
