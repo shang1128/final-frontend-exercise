59bedf574feb634042cad77edbfd6222031c3089
shang1128 <centrii52@gmail.com>
2021-05-12 19:50:33 +0800

    new changes

--- css/sample.js
+++ css/sample.js
@@ -39,23 +39,23 @@ console.log(assignedGrade(63));
 // })
 
 document.getElementsByClassName('black')[0].addEventListener('click', function(){
-    document.getElementById('imageChanges').src="Beats-earphone/beats__3.png";
+    document.getElementById('imageChanges').src="../Beats-earphone/beats__3.png";
 })
 document.getElementsByClassName('red')[0].addEventListener('click', function(){
-    document.getElementById('imageChanges').src="Beats-earphone/beat-red.png";
+    document.getElementById('imageChanges').src="../Beats-earphone/beat-red.png";
 })
 document.getElementsByClassName('blue')[0].addEventListener('click', function(){
-    document.getElementById('imageChanges').src="Beats-earphone/beats__4.png";
+    document.getElementById('imageChanges').src="../Beats-earphone/beats__4.png";
     
 })
 // document.getElementsByClassName('yellow')[0].addEventListener('click', function(){
 //     document.getElementById('imageChanges').src="Beats-earphone/beat-red.png";
 // })
 document.getElementsByClassName('white')[0].addEventListener('click', function(){
-    document.getElementById('imageChanges').src="Beats-earphone/beats__2.png";
+    document.getElementById('imageChanges').src="../Beats-earphone/beats__2.png";
 })
 document.getElementsByClassName('peach')[0].addEventListener('click', function(){
-    document.getElementById('imageChanges').src="Beats-earphone/beats__1.png";
+    document.getElementById('imageChanges').src="../Beats-earphone/beats__1.png";
 })
 
 
